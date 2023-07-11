132d35619574bc48dd8228dfc226745660576256
parkmintchoco <pyh1111802@naver.com>
2023-07-11 17:44:24 +0900

    last

--- more_info/more_info-script.js
+++ more_info/more_info-script.js
@@ -1,14 +1,9 @@
-var button = document.getElementById('mainConTentContectbutton');
+var button = document.getElementById('maincontent1button1');
 button.addEventListener('click', function() {
   window.location.href = 'https://l.instagram.com/?u=https%3A%2F%2Fopen.kakao.com%2Fo%2FgveiQKuf&e=AT1YcGPQrij-CstaTU9tMp2cT2a5-ZMjHp8nP2qgkopnPY2303_aEag8JBgZFqHd8Jy9MxGGqUhq8SBfYRqqpC78cABO5O1Xk-ztHEZCKILxZf8uXmzAQdg6iFJ-2vXnx3bgfOeeMXCAOLFGpEGhmw';
 });
 
-var button = document.getElementById('mainContent3Contect1ContentLink1button');
-button.addEventListener('click', function() {
-  window.location.href = '/more_info/more_info-html.html';
-});
-
-var button = document.getElementById('mainContent3contect2ContentLinkbutton');
+var button = document.getElementById('maincontent2button2');
 button.addEventListener('click', function() {
   window.location.href = 'https://l.instagram.com/?u=https%3A%2F%2Fopen.kakao.com%2Fo%2FgveiQKuf&e=AT1YcGPQrij-CstaTU9tMp2cT2a5-ZMjHp8nP2qgkopnPY2303_aEag8JBgZFqHd8Jy9MxGGqUhq8SBfYRqqpC78cABO5O1Xk-ztHEZCKILxZf8uXmzAQdg6iFJ-2vXnx3bgfOeeMXCAOLFGpEGhmw';
 });
