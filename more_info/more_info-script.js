4bdcb88fed850c35740ce8a61e55924fc2651983
parkmintchoco <pyh1111802@naver.com>
2023-10-12 12:06:06 +0900

    final website

--- more_info/more_info-script.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById('maincontent1button1');
-button.addEventListener('click', function() {
-  window.location.href = 'https://l.instagram.com/?u=https%3A%2F%2Fopen.kakao.com%2Fo%2FgveiQKuf&e=AT1YcGPQrij-CstaTU9tMp2cT2a5-ZMjHp8nP2qgkopnPY2303_aEag8JBgZFqHd8Jy9MxGGqUhq8SBfYRqqpC78cABO5O1Xk-ztHEZCKILxZf8uXmzAQdg6iFJ-2vXnx3bgfOeeMXCAOLFGpEGhmw';
-});
-
-var button = document.getElementById('maincontent2button2');
-button.addEventListener('click', function() {
-  window.location.href = 'https://l.instagram.com/?u=https%3A%2F%2Fopen.kakao.com%2Fo%2FgveiQKuf&e=AT1YcGPQrij-CstaTU9tMp2cT2a5-ZMjHp8nP2qgkopnPY2303_aEag8JBgZFqHd8Jy9MxGGqUhq8SBfYRqqpC78cABO5O1Xk-ztHEZCKILxZf8uXmzAQdg6iFJ-2vXnx3bgfOeeMXCAOLFGpEGhmw';
-});
